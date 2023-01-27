0a4d3327b42257422e80f91f92b56d849f62313e
Aquib Iqbal <aquib.iq.1011@gmail.com>
2023-01-28 01:05:57 +0530

    update

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
-from flask import Flask,render_template
-
-app=Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from keras.models import load_model
+import tensorflow as tf
 
 
-if __name__ == "__main__":
-    app.run()#(debug=False,host='0.0.0.0')
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    quantity = data['quantity']
+    volume = data['volume']
+    distance = data['distance']
+    
+    input_data = np.array([quantity, volume, distance]).reshape(1, -1)
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(input_data)
+    
+    model = tf.keras.models.load_model('model.h5', compile=False)
+
+    prediction = model.predict(scaled_data)
+    
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run()(debug=False,host='0.0.0.0')
